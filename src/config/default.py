a073c7ea362a79cd427daf75425f6e09bb3d9f90
macLeHoang <lehoang.20092000@gmail.com>
2023-10-21 17:27:50 +0700

    add detection

---
+++ src/config/default.py
@@ -0,0 +1,23 @@
+from yacs.config import CfgNode as CN
+
+_C = CN()
+
+# -----------------------------------------------------------------------------
+# DETECTION TASK CONFIG
+# -----------------------------------------------------------------------------
+_C.DETECT = CN()
+
+_C.DETECT.MODEL = '' # model path
+_C.DETECT.SZ = 640 # input size
+_C.DETECT.METHOD = 'sliding_window' # inference mode: normal
+                                  #                   grid
+                                  #                   silding_window   
+
+# used for grid inference mode
+_C.DETECT.GRID_SIZE = 640 # int or tuple(W, H)
+
+# used for sliding window mode 
+_C.DETECT.WINDOW_SIZE = 640 # int or tuple
+_C.DETECT.OVERLAP_WINSIZE = 0.7 # float or tuple, must be in-range (0, 1)
+_C.DETECT.AREA_NMS_THRESH = 0.9 # float, must be in-range (0, 1)
+
---
+++ src/detection/base.py
@@ -0,0 +1,213 @@
+import numpy as np
+import math
+from multiprocessing import Pool
+
+
+class BaseInfrence:
+    def load_model(self, cfg, **kwargs):
+        """Load Detection model
+        Parameters:
+        -----------
+        cfg, CfgNode instance
+            param config
+        """
+        raise NotImplementedError
+
+
+    def detect(self, batch, **kwargs):
+        """Normal inference model
+        Parameters:
+        -----------
+        batch, b x [(H, W, 3)]:
+            list of input image
+
+        Returns:
+        ----------- 
+        results, b x [(n, 6)]:
+            list of detection of each batch
+        """
+        raise NotImplementedError
+
+
+    def normal_infer(self, batch, **kwargs):
+        """Normal model inference 
+        Parameters:
+        -----------
+        batch, b x [(H, W, 3)]:
+            list of input image
+        """
+        return self.detect(batch, **kwargs)
+    
+
+    def grid_infer(self, batch, cfg, **kwargs):
+        """Grid inference 
+        Dividing input image into patches -> making detection on each patch -> remap 
+
+        Parameters:
+        -----------
+        batch, b x [(H, W, 3)]:
+            list of input image
+
+        cfg, CfgNode instance
+            param config
+
+        Returns:
+        -----------
+        The list containing the detection result of each image in batch
+        """
+        batch_shape = [img.shape[:2] for img in batch]
+        batch_shape = np.array(batch_shape).reshape(-1, 2) # shape (N, 2)
+        grid_sz = cfg.DETECT.GRID_SIZE
+        if isinstance(grid_sz, int):
+            grid_sz = (grid_sz, grid_sz)
+        grid_sz = np.array([grid_sz]).astype(np.int32) # shape (1, 2)
+
+        num_grids = np.ceil(batch_shape/grid_sz).astype(np.int32) # number of grids in h- and w- dims 
+                                                                  # shape (N, 2)
+        total_num_grid_each_img = num_grids[:, 0]*num_grids[:, 1] # total number of girds of each image
+        cumsum_num_grid = np.cumsum(total_num_grid_each_img) # cummulative number of grids from first image in batch
+        padded_shapes = [(int(num_grid[0]*grid_sz[0]), 
+                          int(num_grid[1]*grid_sz[1]), 
+                          3) for num_grid in num_grids] # padded shape for each image in batch
+
+        patches=[]
+        for idx, img in enumerate(batch):
+            padded_frame = np.zeros(shape=padded_shapes[idx])
+            h, w = img.shape[:2]
+            padded_frame[:h, :w] = img
+            num_grid = num_grids[idx] # the number of grids of image at index idx
+            for i in range(num_grid[0]):
+                for j in range(num_grid[1]):
+                    patches.append(padded_frame[int(i*grid_sz[0]): int((i+1)*grid_sz[0]),
+                                                int(j*grid_sz[1]): int((j+1)*grid_sz[1]), ])
+        
+        remap = []
+        results = self.detect(patches, **kwargs)
+        for idx, cs_num_grid in enumerate(cumsum_num_grid):
+            # the detection result of one image
+            if idx == 0:
+                result = results[:cs_num_grid]
+            else:
+                result = results[cumsum_num_grid[idx-1]: cumsum_num_grid[idx]]
+
+            # re-map to the original coor
+            r = []
+            for idx_result, pred in enumerate(result):
+                num_grid = num_grids[idx]
+                j, i = idx_result % num_grid[1], idx_result // num_grid[1]
+                pred[:, [0, 2]] += j*grid_sz[1]
+                pred[:, [1, 3]] += i*grid_sz[0]
+                r += pred.tolist()
+            remap.append(r)
+        return remap
+    
+
+    def sliding_window_infer(self, batch, cfg, **kwargs):
+        """Sliding window model inference 
+        Sliding window through image -> detect in each window -> remap
+        
+        Parameters:
+        -----------
+        batch, b x [(H, W, 3)]:
+            list of input image
+
+        cfg, CfgNode instance
+            param config
+        
+        Returns:
+        -----------
+        The list containing the detection result of each image in batch
+        """
+        batch_shape = [img.shape[:2] for img in batch]
+        batch_shape = np.array(batch_shape).reshape(-1, 2) # shape (N, 2)
+        window_sz = cfg.DETECT.WINDOW_SIZE
+        if isinstance(window_sz, int):
+            window_sz = (window_sz, window_sz)
+        window_sz = np.array([window_sz]).astype(np.int32)
+
+        overlap_ratio = cfg.DETECT.OVERLAP_WINSIZE
+        step_size = [int((1-overlap_ratio) * size) for size in window_sz] # non-overlap size in h- and w- dim
+                                                                          # shape (2,)
+        num_grid_h = (batch_shape[:, 0] - window_sz[0]) / step_size[0] + 1 # shape (N, )
+        num_grid_w = (batch_shape[:, 1] - window_sz[1]) / step_size[1] + 1
+        left_out_h = batch_shape[:, 0] - window_sz[0] - np.int32(num_grid_h-1)*step_size[0]
+        left_out_w = batch_shape[:, 1] - window_sz[1] - np.int32(num_grid_w-1)*step_size[1]
+
+        total_num_grid_each_batch = np.int32(np.ceil(num_grid_h)*np.ceil(num_grid_w)) # shape (N, )
+        cumsum_num_grid = np.cumsum(total_num_grid_each_batch) # cummulative number of grids from first image
+        padded_shapes = [(int(bz[0] + step_size[0] - left_out_h[i]), 
+                          int(bz[1] + step_size[1] - left_out_w[i]), 
+                          3) for i, bz in enumerate(batch_shape)]
+        patches=[]
+        for idx, img in enumerate(batch):
+            padded_frame = np.zeros(shape=padded_shapes[idx])
+            h, w = img.shape[:2]
+            padded_frame[:h, :w] = img
+            for i in range(int(math.ceil(num_grid_h[idx]))):
+                for j in range(int(math.ceil(num_grid_w[idx]))):
+                    patches.append(padded_frame[i*step_size[0]: i*step_size[0] + window_sz[0],
+                                                j*step_size[1]: j*step_size[1] + window_sz[1], ])
+        
+        remap = []
+        results = self.detect(patches, **kwargs)
+        for idx, cs_num_grid in enumerate(cumsum_num_grid):
+            if idx == 0:
+                result = results[:cs_num_grid]
+            else:
+                result = results[cumsum_num_grid[idx-1]: cumsum_num_grid[idx]]
+            
+            # re-map to the original coor
+            r=[]
+            for idx_result, pred in enumerate(result):
+                j, i = idx_result % (int(num_grid_w[idx]+1)), idx_result // (int(num_grid_w[idx]+1))
+                pred[:, [0, 2]] += j*step_size[1]
+                pred[:, [1, 3]] += i*step_size[0]
+                r += pred.tolist()
+            remap.append(r)
+
+        assert len(remap) == len(batch)
+        overlap_area_thresh = cfg.DETECT.AREA_NMS_THRESH
+        with Pool() as p:
+            NMS_remap = p.starmap(area_NMS, zip(remap, 
+                                                [overlap_area_thresh for _ in range(len(remap))]))
+        return NMS_remap
+
+
+def area_NMS(preds, overlap_area_thresh=0.7):
+    '''Non-max supression redundant bounding-boxes 
+    A box that lies inside or nearly inside the other boxes  
+    (e.g. area overlapping > t%) is removed  
+    '''
+    preds = list(sorted(preds, key=lambda x: (x[2]-x[0])*(x[3]-x[1]), reverse=True))
+    preds = np.array(preds).reshape(-1, 1, 6)
+
+    keep = preds[0:1]
+    for idx, bb in enumerate(preds[1:], start=1):
+        bb = np.array(bb).reshape(-1, 1, 6).repeat(keep.shape[0], axis=0)
+        bbes = np.concatenate([keep, bb], axis=1) # shape N x 2 x 4
+        
+        # calculate IOU of current box with all boxes in keep 
+        xmin = np.max(bbes[..., 0], axis=1) # shape N,
+        ymin = np.max(bbes[..., 1], axis=1)
+        xmax = np.min(bbes[..., 2], axis=1)
+        ymax = np.min(bbes[..., 3], axis=1)
+        request_bb = np.concatenate([xmin[:, None],
+                                      ymin[:, None],
+                                      xmax[:, None],
+                                      ymax[:, None]], axis=1) #shape N x 4
+        check = (xmin < xmax) & (ymin < ymax)
+        request_bb = request_bb[check] # Keep valid IOU values
+        if request_bb.shape[0]==0:
+            keep = np.concatenate([keep, preds[idx:idx+1]], axis=0)
+            continue
+
+        request_side = request_bb[..., [2, 3]] - request_bb[..., [0, 1]]
+        request_area = request_side[:, 0]*request_side[:, 1]
+        bb_area = (bb[0, 0, 2]-bb[0, 0, 0])*(bb[0, 0, 3]-bb[0, 0, 1])
+        ratio = request_area / bb_area
+        check = ratio > overlap_area_thresh
+        if np.any(check): 
+            continue
+        keep = np.concatenate([keep, preds[idx:idx+1]], axis=0)
+    
+    return keep[:, 0]
